7c491e41714ec719790743d7b1a968dea2b5a8b7
meakitfed <nathan.bonnard@hotmail.fr>
2018-11-23 16:35:41 +0100

    Ajoutez des fichiers projet.

---
+++ IntranetPOPS1819/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IntranetPOPS1819.Startup))]
+namespace IntranetPOPS1819
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
